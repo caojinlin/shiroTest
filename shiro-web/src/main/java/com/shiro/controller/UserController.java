5333dde1a580eb8d70cff144a945eb5bbcdc4d1a
caojinlin <caojinlin563580@gmail.com>
2018-05-09 13:28:05 +0800

    shiro学习,redis自动登录

--- shiro-web/src/main/java/com/shiro/controller/UserController.java
+++ shiro-web/src/main/java/com/shiro/controller/UserController.java
@@ -22,6 +22,8 @@ public class UserController {
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(),user.getPassword());
         try {
+            //保存登陆状态
+            token.setRememberMe(true);
             subject.login(token);
         } catch (AuthenticationException e) {
             return e.getMessage();
